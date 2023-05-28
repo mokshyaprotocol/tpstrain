2804ab541d2d3acbe72946386170aa0e74397fe5
sabin <sabin@noveltytechnology.com>
2023-05-28 14:26:44 +0545

    desing fixes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,107 @@
-import "./App.css";
-import Train from "./components/Train";
-import Bottom from "./components/Bottom";
-import CustomModal from "./components/CustomModal";
-import { useEffect, useState } from "react";
-import { DONATION, LEADERBOARD } from "./constants";
-import LeaderBoardTable from "./components/LeaderBoardTable";
-import DonationForm from "./components/DonationForm";
-import { FaMedal } from "react-icons/fa";
-import Logo from "./images/logo.png";
-import Loading from "./components/Loading";
-import Congratulations from "./components/Congratulations";
+import './App.css';
+import Train from './components/Train';
+import Bottom from './components/Bottom';
+import CustomModal from './components/CustomModal';
+import { useEffect, useState } from 'react';
+import { DONATION, LEADERBOARD } from './constants';
+import LeaderBoardTable from './components/LeaderBoardTable';
+import DonationForm from './components/DonationForm';
+import { FaMedal } from 'react-icons/fa';
+import Logo from './images/logo.png';
+import Loading from './components/Loading';
+import Congratulations from './components/Congratulations';
+import { Button } from 'antd';
+
+import FlipNumbers from 'react-flip-numbers';
+import NumberAnimation from './components/NumberAnimation';
 
 function App() {
-  const [modalActiveFor, setModalActiveFor] = useState("");
-  const [title, setTitle] = useState("");
-  const [showOk, setShowOk] = useState("");
+  const [modalActiveFor, setModalActiveFor] = useState('');
+  const [title, setTitle] = useState('');
+  const [showOk, setShowOk] = useState('');
   const [hasDonated, setHasDonated] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [donatedAmount, setDonatedAmount] = useState(null);
+
+  const handleDonate = () => {
+    setModalActiveFor(DONATION);
+  };
+
+  const handleLeaderBoard = () => {};
 
   // title
   useEffect(() => {
     setShowOk(hasDonated);
     if (hasDonated) {
-      setTitle("Congratulations!!");
+      setTitle('Congratulations!!');
     } else {
-      setTitle("");
+      setTitle('');
     }
   }, [hasDonated]);
 
   return (
-    <div className="App grid grid-cols-1 items-center justify-items-center w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] 2xl:w-[80%] pt-[1rem]  mx-auto max-h-[100vh] overflow-hidden">
-      <img
-        src={Logo}
-        alt="logo"
-        className="object-contain h-[30px] lg:h-[40px]"
-      />
-      <Train />
-      <Bottom
-        modalActiveFor={modalActiveFor}
-        setModalActiveFor={setModalActiveFor}
-      />
+    <div className='tps-train-container'>
+      <div className='tps-train-title'>
+        <img
+          src={Logo}
+          alt='logo'
+          className='object-contain h-[30px] lg:h-[40px] title-logo'
+        />
+      </div>
+      <div className='tps-train-image'>
+        <Train />
+      </div>
+      <div className='donate-text'>
+        <h3 className='text-2xl md:mt-[1rem]' style={{ fontSize: 23 }}>
+          "Donate APt to make the train move faster"
+        </h3>
+      </div>
+      <div className='tps-train-score-btn'>
+        {/* <h2
+          className='font-bold text-4xl mt-[3rem]'
+          style={{ textAlign: 'center' }}
+        >
+          5500
+        </h2> */}
+        <NumberAnimation animationNumber={3000} />
+        <Button
+          onClick={handleDonate}
+          type='ghost'
+          className='bg-primary text-white my-[2.6rem] md:my-[1.9rem]  flex mx-auto justify-center items-center w-[160px] h-[52px] md:w-[200px] lg:w-[270px]  md:h-[50px] lg:h-[60px]'
+        >
+          DONATE NOW
+        </Button>
+      </div>
+      <div className='tps-train-leaderboard'>
+        <p className='text-xl mt-[1rem] text-center' style={{ fontSize: 18 }}>
+          Aptos is super fast. Don’t believe it? Donate APT to stress test.
+        </p>
+        <Button
+          className='mx-auto w-full flex items-center justify-center leaderboard-btn'
+          icon={<FaMedal className='text-[#E8E254] font-extrabold text-3xl ' />}
+          size='middle'
+          type='ghost'
+          onClick={handleLeaderBoard}
+        >
+          <span className='text-2xl font-semibold'> Leaderboard</span>
+        </Button>
+        <p
+          className='mt-[0.5rem] text-gray-600 text-center'
+          style={{ fontSize: 12 }}
+        >
+          TPS Train V1.6
+        </p>
+        <p className='mt-[0.1rem] text-gray-500 text-center'>
+          &copy; TPS Train
+        </p>
+      </div>
       <CustomModal
         showOk={showOk}
         modalActiveFor={modalActiveFor}
         setHasDonated={setHasDonated}
         setModalActiveFor={setModalActiveFor}
-        title={title}>
+        title={title}
+      >
         {modalActiveFor == DONATION && !hasDonated && !isLoading && (
           <DonationForm
             hasDonated={hasDonated}
@@ -69,6 +124,19 @@ function App() {
         {hasDonated && <Congratulations donatedAmount={donatedAmount} />}
       </CustomModal>
     </div>
+    // <div className="App grid grid-cols-1 items-center justify-items-center w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] 2xl:w-[80%] pt-[1rem]  mx-auto max-h-[100vh] overflow-hidden">
+    // <img
+    //   src={Logo}
+    //   alt="logo"
+    //   className="object-contain h-[30px] lg:h-[40px]"
+    // />
+    //   <Train />
+    //   <Bottom
+    //     modalActiveFor={modalActiveFor}
+    //     setModalActiveFor={setModalActiveFor}
+    //   />
+
+    // </div>
   );
 }
 
--- src/components/Train.js
+++ src/components/Train.js
@@ -1,12 +1,12 @@
-import React from "react";
-import TrainImage from "../images/trainnnn.gif";
+import React from 'react';
+import TrainImage from '../images/trainnnn.gif';
 const Train = () => {
   return (
-    <div>
+    <div style={{ marginBottom: 30 }}>
       <img
         src={TrainImage}
-        alt="image"
-        className="md:h-[250px]  lg:w-[630px] lg:h-[260px]  object-cover"
+        alt='image'
+        className='md:h-[250px]  lg:w-[630px] lg:h-[260px]  object-cover'
       />
     </div>
   );
